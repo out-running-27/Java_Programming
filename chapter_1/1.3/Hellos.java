5746976dfcaa50cd6d4470532cc986d98e7306ea
out-running-27 <paul.russum@gmail.com>
2018-07-04 10:35:18 -0500

    added Hellos

---
+++ chapter_1/1.3/Hellos.java
@@ -0,0 +1,23 @@
+public class Hellos {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 10 == 1 && (10 > i % 100 || i % 100 > 20)) {
+                System.out.print(i + "st");
+            }
+            else if (i % 10 == 2 && (10 > i % 100 || i % 100 > 20)) {
+                System.out.print(i + "nd");
+            }
+            else if (i % 10 == 3 && (10 > i % 100 || i % 100 > 20)) {
+                System.out.print(i + "rd");
+            }
+            else
+            {
+                System.out.print(i + "th");
+            }
+            System.out.println(" Hello");
+
+        }
+    }
+}
